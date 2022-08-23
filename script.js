3033552f2cb7826e2bb7b2d631dd73a67e70feee
Ola <ohagenbendixen@gmail.com>
2022-08-23 16:19:38 +0200

    Add makeGrid Function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('#container');
 
 
-const div = document.createElement('div')
-container.appendChild(div)
+//const div = document.createElement('div')
+//container.appendChild(div)
+
+function makeGrid(gridSize) {
+    const grid = gridSize * gridSize;
+    for(let i = 0; i < grid; i++) {
+        const div = document.createElement('div')
+        container.appendChild(div);
+    }
+}
+makeGrid(16);
