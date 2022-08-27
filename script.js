59e208c5ffe6304621b45a91fafa39c644c68526
Ola <ohagenbendixen@gmail.com>
2022-08-27 12:40:50 +0200

    Add colorGrid function

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ makeGrid(32);
 const gridDivs = document.querySelectorAll('.grid-div')
 console.log(gridDivs)
 
-gridDivs.forEach(div => div.addEventListener('mouseover', colorGrid));
+gridDivs.forEach(div => div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = 'black'
+}));
 
 
 
