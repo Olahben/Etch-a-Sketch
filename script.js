d009fa6b6caac30f589474039a523d464f1a7410
Ola <ohagenbendixen@gmail.com>
2022-08-30 16:42:16 +0200

    Add removeGrid function

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ function makeGrid(gridSize) {
 }
 //makeGrid(16);
 
+function removeGrid(parent) {
+    while (parent.firstChild) { //While there exists a child in the parent node
+        parent.removeChild(parent.firstChild)
+    } 
+}
+
 
 const gridDivs = document.querySelectorAll('.grid-div')
 //console.log(gridDivs)
@@ -29,4 +35,9 @@ console.log(input.value)
 input.addEventListener('change', () => {
     console.log(input.value)
     
-})
+    makeGrid(input.value)
+    container.style.gridTemplateRows = `${input.value} 1fr`;
+    container.style.gridTemplateColumns = `${input.value} 1fr`;
+});
+
+
